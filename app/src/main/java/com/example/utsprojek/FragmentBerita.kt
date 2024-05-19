5e6893096743d5668db92ad1874a1752f220bab3
Tarissazzhr <tarissa1402@gmail.com>
2024-05-19 13:34:32 +0700

    update berita

--- app/src/main/java/com/example/utsprojek/FragmentBerita.kt
+++ app/src/main/java/com/example/utsprojek/FragmentBerita.kt
@@ -27,14 +27,12 @@ class FragmentBerita : Fragment() {
 
         listView = view.findViewById(R.id.listView)
         val beritaList = listOf(
-            DataBerita("PNJ KEMBALI MENDAPATKAN OPINI WAJAR TANPA PENGECUALIAN PADA LHP AUDIT KAP PADA LK 2023", "Kementerian Komunikasi dan Informatika (Kominfo) menyebut pihaknya akan melakukan prosedur standar untuk menelusuri dugaan kebocoran data Kependudukan dan Pencatatan Sipil (Dukcapil).",
-                "Sebelumnya, sebanyak 337 juta data masyarakat di Direktorat Dukcapil Kementerian Dalam Negeri (Kemendagri) diduga mengalami kebocoran dan dijual di forum online hacker BreachForums.\n" +
+            DataBerita("PNJ KEMBALI MENDAPATKAN OPINI WAJAR TANPA PENGECUALIAN PADA LHP AUDIT KAP PADA LK 2023", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus volutpat blandit facilisis. Etiam massa massa, ultrices ac sagittis non, euismod eget erat. Praesent suscipit fringilla sapien, eget dapibus enim tristique dapibus.",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus volutpat blandit facilisis. Etiam massa massa, ultrices ac sagittis non, euismod eget erat. Praesent suscipit fringilla sapien, eget dapibus enim tristique dapibus. Sed finibus ipsum ac ante laoreet, vel condimentum ex fermentum.\n" +
                         "\n" +
-                        "Kebocoran itu diungkap Teguh Aprianto, pendiri Ethical Hacker Indonesia, di media sosial pada Minggu (16/7). Teguh menjelaskan data yang dipastikan bocor adalah nama, Nomor Induk Kependudukan (NIK), nomor Kartu Keluarga (KK), tanggal lahir, alamat, nama ayah, nama ibu, NIK ibu, nomor akta lahir, nomor akta nikah dan lainnya.", "https://example.com/image1.jpg"),
-            DataBerita("Tarif Rp24 Ribu Rute Terjauh LRT Dinilai Wajar", "PENGAMAT transportasi Djoko Setijowarno mengungkapkan tarif LRT Jabodebek yang diputuskan oleh Kementerian Perhubungan sebesar Rp24 ribu untuk rute terjauh dari Stasiun Harjamukti ke Dukuh Atas sudah wajar dan sesuai.",
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus volutpat blandit facilisis. Etiam massa massa, ultrices ac sagittis non, euismod eget erat. Praesent suscipit fringilla sapien, eget dapibus enim tristique dapibus. Sed finibus ipsum ac ante laoreet, vel condimentum ex fermentum.", "https://example.com/image1.jpg"),
+            DataBerita("PNJ KEMBALI MENDAPATKAN OPINI WAJAR", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus volutpat blandit facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus volutpat blandit facilisis.",
                 "Deskripsi Berita 1 untuk DetailActivity", "https://example.com/image1.jpg"),
-            DataBerita("Indonesia Juara SEA V League 2023 Pekan Pertama", "TIMNAS voli putra Indonesia berhasil meraih gelar juara SEA V League 2023 pekan pertama usai mengalahkan Thailand 3-1 (21-25, 25-17, 25-23, dan 27-25) di GOR Candradimuka Padepokan Voli Jenderal Polisi Kunarto, Sentul, Bogor, Minggu (23/7) malam.", "Deskripsi Berita 1 untuk DetailActivity", "https://example.com/image1.jpg"),
-            DataBerita("Bandara Soekarno-Hatta Direvitalisasi, Catat Area yang Terdampak", "Bandara Soekarno-Hatta, Tangerang, Banten akan direvitalisasi selama sekitar enam bulan ke depan. Hal ini dilakukan untuk meningkatkan kapasitas dan produktivitas pergerakan penumpang dan pesawat.", "Deskripsi Berita 1 untuk DetailActivity", "https://example.com/image1.jpg")
             // Tambahkan item berita lainnya di sini
         )
 
